78ce07e6ec0162cd6994a515751a6ad9b97919ad
Matt Rose <matt@glaikit.co.uk>
2018-11-28 14:29:15 +0000

    Possible fix for #2 - Rearrange View options to be clearer and allow label after the checkbox

--- content-author-accessibility-preview.php
+++ content-author-accessibility-preview.php
@@ -67,25 +67,9 @@ function caa11yp_settings_init() {
   );
 
   add_settings_field(
-    'caa11yp_options[allpages]',
-    __( 'Show site-wide', 'caa11yp' ),
-    'caa11yp_allpages_cb',
-    'caa11yp',
-    'caa11yp_section_visibility'
-  );
-
-  add_settings_field(
-    'caa11yp_options[preview]',
-    __( 'Show in Preview', 'caa11yp' ),
-    'caa11yp_preview_cb',
-    'caa11yp',
-    'caa11yp_section_visibility'
-  );
-
-  add_settings_field(
-    'caa11yp_options[customizer]',
-    __( 'Show in Customizer', 'caa11yp' ),
-    'caa11yp_customizer_cb',
+    'caa11yp_options[views]',
+    __( 'Show on specific views', 'caa11yp' ),
+    'caa11yp_views_cb',
     'caa11yp',
     'caa11yp_section_visibility'
   );
@@ -106,27 +90,18 @@ add_action( 'admin_init', 'caa11yp_settings_init' );
 function caa11yp_section_visibility_cb( $args ) {
 }
 
-function caa11yp_allpages_cb( $args ) {
+function caa11yp_views_cb( $args ) {
   $options = get_option( 'caa11yp_options' );
   $allpages = ( isset( $options['allpages'] ) ) ? $options['allpages'] : 0;
-  ?>
-  <input type="checkbox" name="caa11yp_options[allpages]" value="1" <?php checked( 1, $allpages, true ); ?> />
-  <?php
-}
-
-function caa11yp_preview_cb( $args ) {
-  $options = get_option( 'caa11yp_options' );
   $preview = ( isset( $options['preview'] ) ) ? $options['preview'] : 0;
-  ?>
-  <input type="checkbox" name="caa11yp_options[preview]" value="1" <?php checked( 1, $preview, true ); ?> />
-  <?php
-}
-
-function caa11yp_customizer_cb( $args ) {
-  $options = get_option( 'caa11yp_options' );
   $customizer = ( isset( $options['customizer'] ) ) ? $options['customizer'] : 0;
   ?>
-  <input type="checkbox" name="caa11yp_options[customizer]" value="1" <?php checked( 1, $customizer, true ); ?> />
+  <input type="checkbox" id="caa11yp_options_allpages" name="caa11yp_options[allpages]" value="1" <?php checked( 1, $allpages, true ); ?> />
+  <label for="caa11yp_options_allpages"><?php echo __( 'Show site-wide', 'caa11yp' ); ?></label><br>
+  <input type="checkbox" id="caa11yp_options_preview" name="caa11yp_options[preview]" value="1" <?php checked( 1, $preview, true ); ?> />
+  <label for="caa11yp_options_preview"><?php echo __( 'Show in Preview', 'caa11yp' ); ?></label><br>
+  <input type="checkbox" id="caa11yp_options_customizer" name="caa11yp_options[customizer]" value="1" <?php checked( 1, $customizer, true ); ?> />
+  <label for="caa11yp_options_customizer"><?php echo __( 'Show in Customizer', 'caa11yp' ); ?></label><br>
   <?php
 }
 
